40d006a2271ace44c5e5baa95599c2b10c879a35
SpaceEEC <spaceeec@yahoo.com>
2020-03-27 20:03:30 +0100

    chore: update deps, add new credo rules

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Crux.Rest.MixProject do
   use Mix.Project
 
-  @vsn "0.2.2"
+  @vsn "0.3.0-dev"
   @name :crux_rest
 
   def project do
@@ -10,12 +10,13 @@ defmodule Crux.Rest.MixProject do
       package: package(),
       app: @name,
       version: @vsn,
-      elixir: "~> 1.6",
+      elixir: "~> 1.7",
       description: "Package providing rest functions and rate limiting for the Discord API",
       source_url: "https://github.com/SpaceEEC/#{@name}/",
       homepage_url: "https://github.com/SpaceEEC/#{@name}/",
       deps: deps(),
-      aliases: aliases()
+      aliases: aliases(),
+      docs: docs()
     ]
   end
 
@@ -45,16 +46,38 @@ defmodule Crux.Rest.MixProject do
       {:jason, "~> 1.1"},
       {:ex_doc,
        git: "https://github.com/spaceeec/ex_doc",
-       branch: "feat/umbrella",
+       branch: "fork",
        only: :dev,
        runtime: false},
-      {:credo, "~> 1.1", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.3", only: [:dev, :test], runtime: false}
     ]
   end
 
   defp aliases() do
     [
       docs: ["docs", &generate_config/1]
+    ]
+  end
+
+  defp docs() do
+    [
+      groups_for_callbacks: [
+        User: &(&1[:section] == :user),
+        Channel: &(&1[:section] == :channel),
+        Message: &(&1[:section] == :message),
+        Reaction: &(&1[:section] == :reaction),
+        Guild: &(&1[:section] == :guild),
+        Member: &(&1[:section] == :member),
+        Ban: &(&1[:section] == :ban),
+        Invite: &(&1[:section] == :invite),
+        Emoji: &(&1[:section] == :emoji),
+        Integration: &(&1[:section] == :integration),
+        Webhook: &(&1[:section] == :webhook),
+        Voice: &(&1[:section] == :voice),
+        Gateway: &(&1[:section] == :gateway)
+      ],
+      markdown_processor_options: [breaks: true],
+      formatter: "html"
     ]
   end
 
